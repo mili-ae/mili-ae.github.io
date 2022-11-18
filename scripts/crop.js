7b55a6625743f8b825df518d7191d5161506e62c
mili-ae <gyndenovaa@gmail.com>
2022-11-18 16:23:44 +0400

    Make preview display correct bg and job icon

--- scripts/crop.js
+++ scripts/crop.js
@@ -7,6 +7,27 @@ var image_workspaceSpan = document.querySelector(".image-workspace span");
 var imagePreview_display = document.querySelector(".img-preview img");
 var preview_containerSpan = document.querySelector(".preview-container span");
 
+
+// Prepare canvas
+var canvas;
+setTimeout(() => {
+    canvas = document.getElementById("banner-canvas");
+    canvas.width = 1068;
+    canvas.height = 293; // this is fucking dumb istg
+}, 1000);
+
+//Create map for backgrounds
+const roles = new Map([
+    ["PLD", "tank.png"],
+    ["WAR", "tank.png"],
+    ["DRK", "tank.png"],
+    ["GNB", "tank.png"],
+    ["WHM", "healer.png"],
+    ["AST", "healer.png"],
+    ["SCH", "healer.png"],
+    ["SGE", "healer.png"]
+]);
+
 //Upload image
 actionButton[0].onclick = () => hiddenUpload.click();
 hiddenUpload.onchange = () => {
@@ -27,32 +48,52 @@ hiddenUpload.onchange = () => {
             console.log("Cropper Ready");
             
             image_workspace.addEventListener("cropend", () => {
+                //Get cropped image
                 var croppedImage = this.cropper.getCroppedCanvas().toDataURL("image/png");
-
-                var canvas = document.getElementById("banner-canvas");
-                canvas.width = 1063;
-                canvas.height = 293; // this is fucking dumb istg
+                
                 var ctx = canvas.getContext("2d");
-
-                var background = new Image();
-                var wol = new Image();
-
-                background.src = "images/roles/dps.png";
-                background.onload = function() {
-                     ctx.drawImage(background, 0, 0, 1068, 293);
-                     ctx.globalCompositeOperation = "source-atop";
-
-                     wol.src = croppedImage;
-                     wol.onload = function() {
-                        ctx.drawImage(wol, 550, 0, 500, 290);
-                        var img = canvas.toDataURL("image/png");
-                        imagePreview_display.src = img;
-                    }
-                 }
+                drawBgWol(ctx, croppedImage);
+                drawJobIcon(ctx);
             })
             
         }
     }
-
+    //Initialize cropperjs
     var cropper = new Cropper(image_workspace, options);
+}
+
+function drawBgWol(ctx, croppedImage) {
+    var background = new Image();
+    var wol = new Image();
+    //Change background according to the job type
+    if (roles.has(job.innerText)) {
+        background.src = "images/roles/" + roles.get(job.innerText);
+    }
+    else {
+        background.src = "images/roles/dps.png";
+    }
+    //Paste background on canvas
+    background.onload = function() {
+        ctx.drawImage(background, 0, 0, 1068, 293);
+        //Create mask for wol image
+        ctx.globalCompositeOperation = "source-atop";
+        //Paste cropped wol image
+        wol.src = croppedImage;
+        wol.onload = function() {
+            ctx.drawImage(wol, 550, 0, 500, 290);
+            var img = canvas.toDataURL("image/png");
+            imagePreview_display.src = img;
+        }
+    }
+}
+
+function drawJobIcon(ctx) {
+    var jobIcon = new Image();
+    jobIcon.src = "images/jobs/" + job.innerText.toLowerCase() + ".png";
+    console.log("images/jobs/" + job.innerText.toLowerCase() + ".png");
+    jobIcon.onload = function() {
+        ctx.drawImage(jobIcon, 0, 0);
+        var img = canvas.toDataURL("image/png");
+        imagePreview_display.src = img;
+    }
 }
