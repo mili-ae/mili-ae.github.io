eb4b8220d017f22db60fbb3766eb1a916b4ca5cf
mili-ae <gyndenovaa@gmail.com>
2025-01-13 21:26:30 +0300

    Edit base in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'mili-ae.github.io',
   plugins: [
     vue(),
     vueDevTools(),
